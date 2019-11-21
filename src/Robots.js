f79a12c102b0dfe0f474e83a70a8b616c8f03ea6
Kyle2044 <kyle.marquardt00@yahoo.com>
2019-11-21 13:02:11 -0600

    addingrobofriends

---
+++ src/Robots.js
@@ -0,0 +1,73 @@
+export const robots = [
+{
+	id: 1,
+	name: 'McKenzie Marquardt',
+	username: 'Kenzie',
+	email: 'McKenzie@dogs.com'
+},
+
+{
+	id: 2,
+	name: 'Kyle Marquardt',
+	username: 'Kyle',
+	email: 'Kyle@dogs.com'
+},
+
+{
+	id: 3,
+	name: 'Todd Marquardt',
+	username: 'Todd',
+	email: 'Todd@dogs.com'
+},
+
+{
+	id: 4,
+	name: 'Luke Kimball',
+	username: 'Luke',
+	email: 'Luke@dogs.com'
+},
+
+{
+	id: 5,
+	name: 'Chester Kimabll',
+	username: 'Chester',
+	email: 'Chester@dogs.com'
+},
+
+{
+	id: 6,
+	name: 'Niko Marquardt',
+	username: 'Niko',
+	email: 'NIko@dogs.com'
+},
+
+{
+	id: 7,
+	name: 'Nora Marquardt',
+	username: 'Nora',
+	email: 'Nora@dogs.com'
+},
+
+{
+	id: 8,
+	name: 'Wallie Marquardt',
+	username: 'Wallie',
+	email: 'Wallie@dogs.com'
+},
+
+{
+	id: 9,
+	name: 'Kallie Marquardt',
+	username: 'Kallie',
+	email: 'Kallie@dogs.com'
+},
+
+{
+	id: 10,
+	name: 'Al Court',
+	username: 'Al',
+	email: 'Al@dogs.com'
+},
+
+
+]
